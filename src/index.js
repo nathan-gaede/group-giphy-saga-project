2d22c26c87306b4addfa0ef4662f109616467d41
Nathan Gaede <n.d.gaede@gmail.com>
2022-10-01 12:34:39 -0500

    Added imports and code comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+//Adding necessary imports
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import { takeEvery, put } from "redux-saga/effects";
+import axios from "axios";
 
+
+
+
+//Add Provider for store
 ReactDOM.render(<App />, document.getElementById('root'));
